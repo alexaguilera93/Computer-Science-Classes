d4cb0dfd2646dbbe955f3a376bc0ecb0a6484fce
Alex <alex@debian>
2015-01-29 15:00:29 -0500

    somethings happeneing

--- CS416 Operating System Design/hw0/analyzecache/analyzecache.c
+++ CS416 Operating System Design/hw0/analyzecache/analyzecache.c
@@ -19,25 +19,33 @@ int main(int argc, char *argv[])
 	memset(BigArray, 0, big);
 	int f = 0;
 	while(f < big){
-	     if(f % 2 == 0){
-		BigArray[f] = 'a';
-	     }
-	     else{
-		BigArray[f] = 'b';
-	     }
-	     f++;
+		if(f % 2 == 0){
+			BigArray[f] = 'a';
+		}
+		else{
+			BigArray[f] = 'b';
+		}
+		f++;
 	}
-	
+
 	double power = 1;
 	static double checknum = 0;
 	//traversal to get mapped to physical memory
 	for(power = 1; checknum < big; power++){
 		checknum = pow(2, power);
 	}
+	
 	checknum = 0;
 	for(power = 1; checknum < big; power++){
-                checknum = pow(2, power);
-        }
+		checknum = pow(2, power);
+		step1_t = clock();
+                char a = BigArray[(int)checknum];
+                char b = BigArray[(int)checknum/2];
+                char c = a + b;
+                step2_t = clock();
+                end_t = (double)(step1_t - step2_t) / CLOCKS_PER_SEC;
+                printf("Time for loop was: %f\n", end_t);
+	}
 
 	return 0;
 }
