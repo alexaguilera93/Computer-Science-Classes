f29d27a3a626b430b40b34cf549ea3dcb3e8f834
Alex <alex@debian>
2015-01-29 14:37:38 -0500

    new outline

--- CS416 Operating System Design/hw0/analyzecache/analyzecache.c
+++ CS416 Operating System Design/hw0/analyzecache/analyzecache.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
 #include "analyzecache.h"
 #include <time.h>
 #include <malloc.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+
 
 #define KB 1024
 #define MB 1024 * KB
@@ -12,7 +16,7 @@ int main(int argc, char *argv[])
 	clock_t start_t, end_t, step1_t, step2_t;
 	int big = 8 * MB;
 	char *BigArray = (char*)malloc(big);
-	//memset(BigArray, 0, big);
+	memset(BigArray, 0, big);
 	int f = 0;
 	while(f < big){
 	     if(f % 2 == 0){
@@ -23,14 +27,17 @@ int main(int argc, char *argv[])
 	     }
 	     f++;
 	}
-	int n = 0;
-	int j = 1;
-		while(j + n < big){
-			step1_t = clock();
-			step2_t = clock();
-			j++;
-			n++;
-		}	
 	
+	double power = 1;
+	static double checknum = 0;
+	//traversal to get mapped to physical memory
+	for(power = 1; checknum < big; power++){
+		checknum = pow(2, power);
+	}
+	checknum = 0;
+	for(power = 1; checknum < big; power++){
+                checknum = pow(2, power);
+        }
+
 	return 0;
 }
